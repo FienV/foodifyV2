2fa7530392226dc614f5fbc9c7a041b8f08b1661
Sahsenem <74179971+Sahsenem@users.noreply.github.com>
2021-03-16 14:21:21 +0100

    aanpassingen welcomepage

--- src/app/shared/services/login-client.service.ts
+++ src/app/shared/services/login-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { LoginClient } from '../model/login-client.model';
+/*import { LoginClient } from '../model/login-client.model';*/
 
 @Injectable({
   providedIn: 'root'
