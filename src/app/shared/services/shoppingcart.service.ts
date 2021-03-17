91e54e1f22e43767b953773fa910232df82c6032
bertienschoors <bertienschoors@hotmail.com>
2021-03-17 12:32:26 +0100

    shoppingcart

---
+++ src/app/shared/services/shoppingcart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingcartService {
+ dishes = [];
+  constructor(private http: HttpClient)  {}
+
+  addToShoppingcart(dishes) {
+    this.dishes.push(dishes);
+  }
+
+  getDishes () {
+    return this.dishes;
+  }
+
+  clearShoppingcart () {
+    this.dishes = [];
+    return this.dishes;
+  }
+}
